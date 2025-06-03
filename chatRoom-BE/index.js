d00bd4c6dbfe5a00d9cf90631787093839bfabfb
ShyamPatel0-0 <shyam.mendpara01@gmail.com>
2025-06-04 02:25:48 +0530

    fix(index): mainAapp vaiable typo fixes.

--- chatRoom-BE/index.js
+++ chatRoom-BE/index.js
@@ -2,26 +2,9 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
+const mainApp = require('./app');
 
-const mainAapp = require('./app');
-// const app = express();
-mainAapp.use(cors());
-// app.use(express.json());
+mainApp.use(cors());
 
-
-const server = http.createServer(mainAapp);
+const server = http.createServer(mainApp);
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-// async function fetchMessages() {
-//     const { data, error } = await supabase
-//         .from('room')
-//         .select('name');
-
-//     if (error) {
-//         console.error('Error fetching messages:', error);
-//     } else {
-//         console.log('Rooms:', data);
-//     }
-// }
-
-// fetchMessages();
